681611fc3ee3262d4101f672e5624897b84d9a32
Pedro Henrique <83293805+Pedro-HenriqueWO@users.noreply.github.com>
2022-01-14 13:18:45 -0300

    Update VoltarMenu.svelte

--- Projeto/src/VoltarMenu.svelte
+++ Projeto/src/VoltarMenu.svelte
@@ -9,6 +9,13 @@
 <style>
 	button{
        color:royalblue;
-       cursor: pointer;
+       cursor: pointer;	
+       font: 16px;
+       background-color: #09B93E;
+       padding: 4px 4px;
+       border-radius: 12px;
+       animation: moveBanner 1s 0.1s forwards;
+       margin: 6px;
+       letter-spacing: 0.08rem;
     }
 </style>
