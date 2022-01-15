71d4c755597b92315d9fc50c8e2e987159d4ebd0
Pedro Henrique <83293805+Pedro-HenriqueWO@users.noreply.github.com>
2022-01-15 07:45:41 -0300

    Alteração do botão

--- Projeto/src/VoltarMenu.svelte
+++ Projeto/src/VoltarMenu.svelte
@@ -3,12 +3,20 @@
 </script>
 
 <button on:click={() => trocarEstadoDoJogo('menu')}>
-	Voltar ao menu
+	Voltar ao menu principal
 </button>
 
 <style>
 	button{
-       color:royalblue;
+        font-style:oblique;
+       color:white;
        cursor: pointer;
+       font: 16px;
+       background-color: #5908f0;
+       padding: 4px 4px;
+       border-radius: 12px;
+       animation: moveBanner 1s 0.1s forwards;
+       margin: 6px;
+       letter-spacing: 0.08rem;
     }
 </style>
